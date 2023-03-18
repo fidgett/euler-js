1c04fbe2f6c16056ee6d189a3d54a70c9ddf59d6
fidget <fidget@fidget.ru>
2023-03-18 14:54:40 +0300

    feature(javascript): Решение 4 задачи (#4)

---
+++ javascript/0004.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let maxPalindrome = 0;
+const maxNumber = 999;
+const answer = 906609;
+
+function isPalindrome(num) {
+  const array = num.toString().split("");
+
+  if (array.length < 2) {
+    return false;
+  }
+
+  const reverse = Number(array.reverse().join(""));
+  return num === reverse;
+}
+
+for (let i = 1; i <= maxNumber; i++) {
+  for (let j = 1; j <= maxNumber; j++) {
+    const ij = i * j;
+    if (isPalindrome(ij) && ij > maxPalindrome) {
+      maxPalindrome = ij;
+    }
+  }
+}
+
+if (maxPalindrome === answer) {
+  console.log('success', maxPalindrome);
+} else {
+  console.log('error', maxPalindrome);
+}
+
